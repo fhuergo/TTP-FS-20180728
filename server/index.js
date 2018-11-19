fd31ee94f6bbf69f55e6b971a04bdc3776118f23
Francisco Huergo <franhuergo@gmail.com>
2018-11-19 18:02:59 -0500

    bodyParser

--- server/index.js
+++ server/index.js
@@ -4,3 +4,6 @@ const morgan = require("morgan")
 
 app.use(morgan("dev"))
 app.use(express.static(path.join(__dirname, "..", "./public")))
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
