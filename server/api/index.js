85f24ff24a695efb8589498bdbcd7985b98c8f99
Francisco Huergo <franhuergo@gmail.com>
2018-11-19 18:58:16 -0500

    default send html

--- server/api/index.js
+++ server/api/index.js
@@ -6,4 +6,8 @@ router.use(function(req, res, next) {
   next(err)
 })
 
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html');
+});
+
 module.exports = router
--- server/index.js
+++ server/index.js
@@ -8,3 +8,7 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/api", require("./api"))
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "..", "..", "public", "index.html"))
+})
