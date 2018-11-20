fca7a46f5ac2ee0c38d8731b4a403eba1e36c8e7
Francisco Huergo <franhuergo@gmail.com>
2018-11-20 11:42:31 -0500

    login signup logout routes

--- server/index.js
+++ server/index.js
@@ -3,16 +3,13 @@ const app = express()
 const morgan = require("morgan")
 app.use(morgan("dev"))
 const path = require("path")
-
+const passport = require("passport")
+const session = require("express-session")
 const bodyParser = require("body-parser")
+const db = require("./db") // we will need our sequelize instance from somewhere
 app.use(express.static(path.join(__dirname, "..", "public"))) // can access css, etc
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-const session = require("express-session")
-
-// we will need our sequelize instance from somewhere
-const db = require("./db")
 
 // configure and create our database store
 const SequelizeStore = require("connect-session-sequelize")(session.Store)
@@ -21,6 +18,17 @@ const dbStore = new SequelizeStore({ db: db })
 // sync so that our session table gets created
 dbStore.sync()
 
+passport.serializeUser((user, done) => done(null, user.id))
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findById(id)
+    done(null, user)
+  } catch (err) {
+    done(err)
+  }
+})
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "a wildly insecure secret",
@@ -28,6 +36,9 @@ app.use(
     saveUninitialized: false
   })
 )
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "..", "public", "index.html")) // index.html stays so bundle.js can be accessed!
