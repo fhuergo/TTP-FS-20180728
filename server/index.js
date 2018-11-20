a054f9eddd4076f1308b1dddfafec2add966a0ef
Francisco Huergo <franhuergo@gmail.com>
2018-11-19 19:00:13 -0500

    port added

--- server/index.js
+++ server/index.js
@@ -18,3 +18,10 @@ app.use(function(err, req, res, next) {
   console.error(err.stack)
   res.status(err.status || 500).send(err.message || "Internal server error.")
 })
+
+const port = process.env.PORT || 3000 // this can be very useful if you deploy to Heroku!
+app.listen(port, function() {
+  console.log("Knock, knock")
+  console.log("Who's there?")
+  console.log(`Your server, listening on port ${port}`)
+})
